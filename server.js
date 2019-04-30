b58f1233aa0f55aaadcf1f6c761dbd12b8fb557c
Kevin Yoo <k.ysyoobcit@gmail.com>
2019-04-30 12:59:59 -0700

    fix_2
    cloud db fix

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/thread/:id', async (request, response) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server is up on the port ${port}`);
-    utils.init();
-});
+// app.listen(port, () => {
+//     console.log(`Server is up on the port ${port}`);
+//     utils.init();
+// });
--- utils.js
+++ utils.js
@@ -11,7 +11,7 @@ getObjectId = () => {
 };
 
 init = (callback) => {
-    const client = new MongoClient('mongodb+srv://kev:8ekNBO7U5LHNAOEJ@clusterk-0d6io.mongodb.net/test?retryWrites=true', {useNewUrlParser:true})
+    const client = new MongoClient('mongodb+srv://kev:8ekNBO7U5LHNAOEJ@clusterk-0d6io.mongodb.net/test?retryWrites=true', {useNewUrlParser:true});
     client.connect((err, client) => {
         if (err) {
             return console.log('Unable to connect to DB');
