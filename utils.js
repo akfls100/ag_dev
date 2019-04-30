57340f04d510b0d9a7f204b348ed94cff788460f
Kevin Yoo <k.ysyoobcit@gmail.com>
2019-04-30 13:29:51 -0700

    Atlas deploy

--- utils.js
+++ utils.js
@@ -11,7 +11,8 @@ getObjectId = () => {
 };
 
 init = (callback) => {
-    MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/forumdb', (err, client) => {
+    const client = new MongoClient('mongodb+srv://kev:8ekNBO7U5LHNAOEJ@clusterk-0d6io.mongodb.net/test?retryWrites=true', {useNewUrlParser:true});
+    client.connect((err, client) => {
         if (err) {
             return console.log('Unable to connect to DB');
         }
