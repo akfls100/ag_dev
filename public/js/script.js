25974a0b92966b2ad37a2d22a755d4ea5512e9d5
Kevin Yoo <k.ysyoobcit@gmail.com>
2019-05-04 15:27:46 -0700

    pwd validation

--- public/js/script.js
+++ public/js/script.js
@@ -51,3 +51,65 @@ function show_edit_form() {
 
     form.style.display="block";
 }
+
+
+// Password validation function
+
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on pasword, show message box
+
+myInput.onfocus = function() {
+    document.getElementById("pswmessage").style.display = "block";
+};
+
+// When the user clicks outside password, hide message box
+myInput.onblur = function() {
+    document.getElementById("pswmessage").style.display = "none";
+};
+
+// When the user starts to type password
+myInput.onkeyup = function() {
+    //Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if(myInput.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+    //Validae uppercase letters
+    var upperCaseLetters = /[A-Z]/g;
+    if(myInput.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
+
+    //Validate numbers
+    var numbers = /[0-9]/g;
+    if(myInput.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    // Validate length
+    if(myInput.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
+};
